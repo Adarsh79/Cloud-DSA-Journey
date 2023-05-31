d534acd4fb760225f578173e8290ab717d0d6326
Akarsh <Adarsh.bhaskar7903@gmail.com>
2023-05-31 15:42:51 +0530

    Added replaceChars.py

---
+++ Class/replaceChars.py
@@ -0,0 +1,13 @@
+# Given string we have to remove characters x from the string.
+
+def new_string(s, key):
+    if len(s) == 0:
+        return ""
+    if s[0] == key:
+        return new_string(s[1:], key)
+    return s[0] + new_string(s[1:], key)
+
+
+# string = input("Enter a string: ")
+string = "hello, my name is xAdarsh"
+print(new_string(string, "x"))
