a4612bf04a86de67c0298b2cc93af028bfc5a050
Akarsh <Adarsh.bhaskar7903@gmail.com>
2023-06-01 22:10:29 +0530

    Added climbingStairs.py

---
+++ Class/climbingStairs.py
@@ -0,0 +1,13 @@
+# There are n number of stairs. A girl is standing at the bottom stairs, she can climb 1 or 2 or 3 stairs at a time.
+# Calculate the number of combinations where she can complete climbing all the stairs without climbing extra steps.
+
+def climb_stairs(n):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    return climb_stairs(n - 3) + climb_stairs(n - 2) + climb_stairs(n - 1)
+
+
+num = int(input("Enter the number of stairs: "))
+print(climb_stairs(num))
