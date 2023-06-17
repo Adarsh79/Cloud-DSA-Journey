5956992d494021a5eff25bca970a167161d18f40
Akarsh <Adarsh.bhaskar7903@gmail.com>
2023-06-17 21:48:50 +0530

    Added linkedList.py

---
+++ Class/linkedList.py
@@ -0,0 +1,27 @@
+# Linked List
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.nextAdd = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.temp = None
+
+
+obj = LinkedList()
+
+obj.head = n1 = Node(1)
+n2 = Node(2)
+n1.nextAdd = n2
+n3 = Node(3)
+n2.nextAdd = n3
+
+
+obj.temp = obj.head
+while obj.temp is not None:
+    print(obj.temp.data)
+    obj.temp = obj.temp.nextAdd
