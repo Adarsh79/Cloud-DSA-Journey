ce5de021e2bf46caf9b13c158463cc7610bbc0e1
Akarsh <Adarsh.bhaskar7903@gmail.com>
2023-07-08 20:25:44 +0530

    Update palindrome2SLL.py

--- Class/palindrome2SLL.py
+++ Class/palindrome2SLL.py
@@ -1,66 +1,3 @@
-# class Node:
-#     data = None
-#     next = None
-#
-#     def __init__(self, data):
-#         self.data = data
-#
-#
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-#         self.tail = None
-#
-#     def create(self):
-#         data = int(input("Enter the data: "))
-#         while data != -1:
-#             if self.head is None:
-#                 self.head = self.tail = Node(data)
-#             else:
-#                 newNode = Node(data)
-#                 self.tail.next = newNode
-#                 self.tail = self.tail.next
-#             data = int(input("Enter the data: "))
-#         return self.head
-#
-#     def print(self):
-#         if self.head is None:
-#             return
-#         while self.head:
-#             print(self.head.data, " ", end="")
-#             self.head = self.head.next
-#
-#     def mid(self):
-#         slow = fast = self.head
-#         count = 0
-#         while (fast is not None) and (fast.next is not None):
-#             count += 1
-#             slow = slow.next
-#             fast = fast.next.next
-#         return {"address": slow, "index": count}
-#
-#     def new_linkedlist(self):
-#         temp = self.head
-#         comparison = Node(temp.data)
-#         mid = self.mid()
-#         while temp.next is not mid["address"]:
-#             comparison.data = temp.data
-#             comparison = comparison.next
-#             temp = temp.next
-#
-#         # if comparison is None:
-#         #     return
-#         while comparison:
-#             print(comparison.data, " ", end="")
-#             comparison = comparison.next
-#
-#
-#
-# obj = LinkedList()
-# obj.create()
-# obj.new_linkedlist()
-
-
 class Node:
     data = None
     next = None
@@ -74,6 +11,7 @@ class LinkedList:
         self.head = None
         self.tail = None
 
+    # Creation of linked list
     def create(self):
         data = int(input("Enter the data: "))
         while data != -1:
@@ -86,13 +24,21 @@ class LinkedList:
             data = int(input("Enter the data: "))
         return self.head
 
+    # Function to print the original linked list
     def print(self):
-        if self.head is None:
-            return
-        while self.head:
-            print(self.head.data, " ", end="")
-            self.head = self.head.next
+        self.__helper_print(self.head)
 
+    def __helper_print(self, temp):
+        if temp is None:
+            return
+        print(temp.data, "|", end="")
+        self.__helper_print(temp.next)
+
+    # Function to print the half linked list
+    def display_sub_ll(self, comp):
+        self.__helper_print(comp)
+
+    # Function to find the address and index of the mid-element of a linked list
     def mid(self):
         slow = fast = self.head
         count = 0
@@ -102,23 +48,28 @@ class LinkedList:
             fast = fast.next.next
         return {"address": slow, "index": count}
 
-    def reverse(self):
+    # Function to reverse the inputted linked list
+    def reverse(self, main_head=None):
         prev = None
-        temp = self.head
+        if main_head is None:
+            temp = self.head
+        else:
+            temp = main_head
         while temp is not None:
             trav = temp.next
             temp.next = prev
             prev = temp
             temp = trav
-        self.head = prev
+        if main_head is None:
+            self.head = prev
+            return
+        return prev
 
     def __is_palindrome(self, head, comp):
         if head is None:
             return True
 
-        temp = self.__is_palindrome(head.next, comp.next)
-
-        if temp:
+        if head:
             if comp.next is None:
                 return True
             if head.data == comp.data:
@@ -128,16 +79,24 @@ class LinkedList:
         else:
             return False
 
+    # Function to check if the linked list is palindrome or not
     def is_palindrome(self):
         temp = self.head
-        cur = comparison = Node(temp.data)
+        head2 = comparison = Node(temp.data)
         mid = self.mid()
-        while temp.next is not mid["address"]:
-            comparison.data = temp.data
+        temp = temp.next
+
+        while temp is not mid["address"].next:
+            newNode = Node(temp.data)
+            comparison.next = newNode
             comparison = comparison.next
             temp = temp.next
 
-        return self.__is_palindrome(self.head, cur)
+        head2 = self.reverse(head2)
+
+        self.display_sub_ll(head2)
+
+        return self.__is_palindrome(temp, head2)
 
 
 obj = LinkedList()
