e515d6d52bcbffe2f4dc222b6b90af0397d6ac3e
Akarsh <Adarsh.bhaskar7903@gmail.com>
2023-07-27 20:51:45 +0530

    Added sum_nodes() method

--- Class/binarytree.py
+++ Class/binarytree.py
@@ -69,5 +69,12 @@ def max_min(root):
     return ans
 
 
+def sum_nodes(root):
+    if not root:
+        return 0
+
+    return root.data + sum_nodes(root.left) + sum_nodes(root.right)
+
+
 rootNode = create()
-print(max_min(rootNode))
+print(sum_nodes(rootNode))
