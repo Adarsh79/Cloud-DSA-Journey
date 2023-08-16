a3eb8af92e623413c4d34375720b0215bfcb028a
Adarsh Bhaskar <Adarsh.bhaskar7903@gmail.com>
2023-08-16 21:42:45 +0530

    Added the delete_leaf() and the delete() method

--- Class/trees/bst/completeBST.py
+++ Class/trees/bst/completeBST.py
@@ -30,6 +30,53 @@ def create():
         front.right = newNode
 
 
+def delete_leaf(root, elm):
+    q = [root]
+    while len(q):
+        temp = q.pop(0)
+        if temp is elm:
+            temp = None
+            return
+        if temp.right:
+            if temp.right is elm:
+                temp.right = None
+                return
+            else:
+                q.append(temp.right)
+        if temp.left:
+            if temp.left is elm:
+                temp.left = None
+                return
+            else:
+                q.append(temp.left)
+
+
+def delete(root, key):
+    if root is None:
+        return None
+    if root.left is None and root.right is None:
+        if root.val == key:
+            return None
+        else:
+            return root
+    key_node = None
+    q = [root]
+    temp = None
+    while len(q):
+        temp = q.pop(0)
+        if temp.val == key:
+            key_node = temp
+        if temp.left:
+            q.append(temp.left)
+        if temp.right:
+            q.append(temp.right)
+    if key_node:
+        x = temp.val
+        delete_leaf(root, temp)
+        key_node.val = x
+    return root
+
+
 def bfs(root):
     if not root:
         return
@@ -48,4 +95,9 @@ def bfs(root):
 
 
 newRoot = create()
+bfs(newRoot)
+
+value = int(input("\nEnter the data to be deleted: "))
+deleteData = delete(newRoot, value)
+print("\nThe deleted node is:", deleteData.val)
 bfs(newRoot)
