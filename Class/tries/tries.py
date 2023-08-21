f7165bf6f13027165840c8baac26c88b6858155a
Adarsh Bhaskar <Adarsh.bhaskar7903@gmail.com>
2023-08-21 21:25:16 +0530

    Added tries directory and tries.py

---
+++ Class/tries/tries.py
@@ -0,0 +1,28 @@
+class TrieNode:
+    def __init__(self, data):
+        self.data = data
+        self.child = [None] * 26
+        self.finish = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode('\0')
+
+    def __insert(self, root, word):
+        if word == '':
+            root.finish = True
+            return
+
+        index = ord(word[0]) - ord('a')
+        if root.child[index] is None:
+            newNode = TrieNode(word[0])
+            root.child[index] = newNode
+        self.__insert(root.child[index], word[1:])
+
+    def insertWord(self, word):
+        self.__insert(self.root, word)
+
+
+ob = Trie()
+ob.insertWord("alcohol")
